5ec5c454c9c5797557b00357ab0974833bf76e77
armankoca <armankoca@protonmail.com>
2021-07-03 16:14:15 +0300

    End Of 3.3 Relative Imports

--- demo_reader/compressed/bzipped.py
+++ demo_reader/compressed/bzipped.py
@@ -1,9 +1,7 @@
 import bz2
-import sys
+from .. import util
 
 opener = bz2.open
 
 if __name__ == "__main__":
-    f = bz2.open(sys.argv[1], mode="wt")
-    f.write(' '.join(sys.argv[2:]))
-    f.close()
+    util.writer.main(opener)
